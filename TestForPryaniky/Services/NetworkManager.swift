294462f45cc155f0099e8f5bf0178924afce227b
Ruslan <ruslan.shtybaev@gmail.com>
2022-05-27 10:26:52 +0300

    setup tableView logic

--- TestForPryaniky/Services/NetworkManager.swift
+++ TestForPryaniky/Services/NetworkManager.swift
@@ -12,15 +12,16 @@ enum Links: String {
 }
 
 class NetworkManager {
-    let shared = NetworkManager()
+    static let shared = NetworkManager()
     
     private init() {}
     
     func fetch(from url: String, and complition: @escaping(Sample) -> Void) {
-        guard let url = URL(string: url) else { return }
+        guard let url = URL(string: url) else { return print(4)}
         
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else {
+                print(3)
                 print(error.debugDescription)
                 return
             }
@@ -29,9 +30,11 @@ class NetworkManager {
                 let sample = try JSONDecoder().decode(Sample.self, from: data)
                 
                 DispatchQueue.main.async {
+                    print(1)
                     complition(sample)
                 }
             } catch let error {
+                print(2)
                 print(error)
             }
         }.resume()
